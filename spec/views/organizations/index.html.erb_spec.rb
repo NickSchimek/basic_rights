d69969397a1d7e420bab9aab8d48e3922de52e7c
Nick Schimek <cfme00@gmail.com>
2019-08-20 19:51:08 -0700

    Generate scaffold for organization resource

---
+++ spec/views/organizations/index.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "organizations/index", type: :view do
+  before(:each) do
+    assign(:organizations, [
+      Organization.create!(
+        :name => "Name",
+        :contact => "Contact",
+        :email => "Email",
+        :phone => "Phone"
+      ),
+      Organization.create!(
+        :name => "Name",
+        :contact => "Contact",
+        :email => "Email",
+        :phone => "Phone"
+      )
+    ])
+  end
+
+  it "renders a list of organizations" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Contact".to_s, :count => 2
+    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    assert_select "tr>td", :text => "Phone".to_s, :count => 2
+  end
+end
