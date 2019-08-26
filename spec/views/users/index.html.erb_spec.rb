27face03db7387e6e60d1ac573130d07420a54f7
Nick Schimek <cfme00@gmail.com>
2019-08-25 21:23:58 -0700

    Update view tests
    Updated so that it passes validations.
    
    Fixes #6

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -4,22 +4,22 @@ RSpec.describe "users/index", type: :view do
   before(:each) do
     assign(:users, [
       User.create!(
-        :name => "Name",
-        :email => "Email",
-        :phone => "Phone"
+        :name => "John",
+        :email => "john@example.org",
+        :phone => "503-555-5555"
       ),
       User.create!(
-        :name => "Name",
-        :email => "Email",
-        :phone => "Phone"
+        :name => "Sue",
+        :email => "sue@example.org",
+        :phone => "503-555-5555"
       )
     ])
   end
 
   it "renders a list of users" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Email".to_s, :count => 2
-    assert_select "tr>td", :text => "Phone".to_s, :count => 2
+    assert_select "tr>td", :text => /John|Sue/, :count => 2
+    assert_select "tr>td", :text => /example.org/, :count => 2
+    assert_select "tr>td", :text => "503-555-5555", :count => 2
   end
 end
