dee59481805d03babdb0965bc999a8e6cb409866
Nick Schimek <cfme00@gmail.com>
2019-08-25 21:22:34 -0700

    Validate uniqueness of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
   end
 end
