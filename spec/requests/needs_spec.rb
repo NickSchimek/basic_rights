c4aa0b6b367ac74894240d97f20ba72f5617ef84
Nick Schimek <cfme00@gmail.com>
2019-08-30 00:25:35 -0700

    Created nested resource organization/needs

--- spec/requests/needs_spec.rb
+++ spec/requests/needs_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Needs", type: :request do
-  describe "GET /needs" do
+  describe "GET organization/needs" do
+    let(:organization) { create(:organization) }
     it "works! (now write some real specs)" do
-      get needs_path
+      get organization_needs_path(organization)
       expect(response).to have_http_status(200)
     end
   end
--- spec/routing/needs_routing_spec.rb
+++ spec/routing/needs_routing_spec.rb
@@ -3,36 +3,36 @@ require "rails_helper"
 RSpec.describe NeedsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/needs").to route_to("needs#index")
+      expect(:get => "organizations/1/needs").to route_to("needs#index", organization_id: '1')
     end
 
     it "routes to #new" do
-      expect(:get => "/needs/new").to route_to("needs#new")
+      expect(:get => "organizations/1/needs/new").to route_to("needs#new", organization_id: '1')
     end
 
     it "routes to #show" do
-      expect(:get => "/needs/1").to route_to("needs#show", :id => "1")
+      expect(:get => "organizations/1/needs/1").to route_to("needs#show", id: '1', organization_id: '1')
     end
 
     it "routes to #edit" do
-      expect(:get => "/needs/1/edit").to route_to("needs#edit", :id => "1")
+      expect(:get => "organizations/1/needs/1/edit").to route_to("needs#edit", id: '1', organization_id: '1')
     end
 
 
     it "routes to #create" do
-      expect(:post => "/needs").to route_to("needs#create")
+      expect(:post => "organizations/1/needs").to route_to("needs#create", organization_id: '1')
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/needs/1").to route_to("needs#update", :id => "1")
+      expect(:put => "organizations/1/needs/1").to route_to("needs#update", id: '1', organization_id: '1')
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/needs/1").to route_to("needs#update", :id => "1")
+      expect(:patch => "organizations/1/needs/1").to route_to("needs#update", id: '1', organization_id: '1')
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/needs/1").to route_to("needs#destroy", :id => "1")
+      expect(:delete => "organizations/1/needs/1").to route_to("needs#destroy", id: '1', organization_id: '1')
     end
   end
 end
