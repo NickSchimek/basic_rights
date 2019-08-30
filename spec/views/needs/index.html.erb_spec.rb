1a0f9d39e67a7432ac8387e8a80ab89517dcb008
Nick Schimek <cfme00@gmail.com>
2019-08-30 12:21:32 -0700

    Update views to use nested resource URI

--- spec/views/needs/index.html.erb_spec.rb
+++ spec/views/needs/index.html.erb_spec.rb
@@ -2,27 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "needs/index", type: :view do
   before(:each) do
-    assign(:needs, [
-      Need.create!(
-        :resource => "Resource",
-        :description => "Description",
-        :quantity => 2,
-        :active => false
-      ),
-      Need.create!(
-        :resource => "Resource",
-        :description => "Description",
-        :quantity => 2,
-        :active => false
-      )
-    ])
+    @needs = assign(:needs, create_list(:need, 2))
+    @needs[1].organization = @needs[0].organization
+    assign(:organization, @needs[0].organization)
   end
 
-  it "renders a list of needs" do
+  it "renders an organizations list of needs" do
     render
-    assert_select "tr>td", :text => "Resource".to_s, :count => 2
-    assert_select "tr>td", :text => "Description".to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
+    assert_select "tr>td", :text => "Resource", :count => 2
+    assert_select "tr>td", :text => "Description", :count => 2
+    assert_select "tr>td", :text => 10.to_s, :count => 2
+    assert_select "tr>td", :text => true.to_s, :count => 2
   end
 end
--- spec/views/needs/show.html.erb_spec.rb
+++ spec/views/needs/show.html.erb_spec.rb
@@ -2,19 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "needs/show", type: :view do
   before(:each) do
-    @need = assign(:need, Need.create!(
-      :resource => "Resource",
-      :description => "Description",
-      :quantity => 2,
-      :active => false
-    ))
+    assign(:need, create(:need))
+    assign(:organization, create(:organization))
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(/Resource/)
     expect(rendered).to match(/Description/)
-    expect(rendered).to match(/2/)
-    expect(rendered).to match(/false/)
+    expect(rendered).to match(/10/)
+    expect(rendered).to match(/true/)
   end
 end
