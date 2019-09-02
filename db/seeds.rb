c6fa45c805056dbe4c49b8e1fd176953fc847a8c
Nick Schimek <cfme00@gmail.com>
2019-09-01 22:52:26 -0700

    Create seeds data (#32)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+info_211 = Organization.create(name: '211_info', contact: 'Ciara', email: 'ciara@example.org', phone: '(503) 555-5555')
+
+info_211.needs.create(resource: "Children's clothing", description: 'All sizes', quantity: '50')
+info_211.needs.create(resource: "Women's clothing", description: 'All sizes', quantity: '25')
+info_211.needs.create(resource: 'Blankets', description: 'All sizes', quantity: '100')
+
+info_211.surplus.create(resource: 'Food', description: 'Canned goods', quantity: '500')
+info_211.surplus.create(resource: 'Mens shoes', description: 'All sizes', quantity: '100')
+info_211.surplus.create(resource: "Women's shoes", description: 'Size 10', quantity: '20')
