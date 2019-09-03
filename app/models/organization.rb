9ed956750a98c4ae46749c299e19dfda4e4ef93e
Nick Schimek <cfme00@gmail.com>
2019-09-02 22:08:12 -0700

    Organization has many claims

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,5 @@ class Organization < ApplicationRecord
   validates :name, presence: true
   has_many :needs, dependent: :destroy
   has_many :surplus, dependent: :destroy
+  has_many :claims, dependent: :destroy
 end
