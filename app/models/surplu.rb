acfcc3dd29e3874bc10481bf7a7aab67cb55b818
Nick Schimek <cfme00@gmail.com>
2019-09-07 21:51:41 -0700

    Surplus has many claims

--- app/models/surplu.rb
+++ app/models/surplu.rb
@@ -1,4 +1,5 @@
 class Surplu < ApplicationRecord
   validates :resource, presence: true
   belongs_to :organization
+  has_many :claims, dependent: :destroy
 end
--- spec/models/surplu_spec.rb
+++ spec/models/surplu_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe Surplu, type: :model do
 
   describe 'relationships' do
     it { should belong_to(:organization) }
+    it { should have_many(:claims).dependent(:destroy) }
   end
 end
