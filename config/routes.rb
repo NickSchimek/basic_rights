1e7532d7c65cddbe89ec590a9be23a0d306fd73e
Nick Schimek <cfme00@gmail.com>
2019-09-16 13:39:15 -0700

    Nest fulfillment within a scoped needs resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :fulfillments
   root 'site_pages#home'
   get 'about', to: 'site_pages#about'
   get 'contact', to: 'site_pages#contact'
@@ -9,11 +8,14 @@ Rails.application.routes.draw do
   resources :users
 
   resources :organizations do
-    resources :needs
+    resources :needs, only: [:index, :new, :create]
     resources :surplus, only: [:index, :new, :create]
     get 'claims', to: 'claims#org_index'
   end
   resources :surplus, only: [:show, :edit, :update, :destroy] do
     resources :claims
+  end
+  resources :needs, only: [:show, :edit, :update, :destroy] do
+    resources :fulfillments
   end
 end
