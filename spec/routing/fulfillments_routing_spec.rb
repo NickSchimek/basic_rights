f6e2dc6ba202cdc164c415a8793e4fda9c10aedb
Nick Schimek <cfme00@gmail.com>
2019-09-16 13:38:22 -0700

    Spec fulfillment routes

--- spec/routing/fulfillments_routing_spec.rb
+++ spec/routing/fulfillments_routing_spec.rb
@@ -3,36 +3,36 @@ require "rails_helper"
 RSpec.describe FulfillmentsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/fulfillments").to route_to("fulfillments#index")
+      expect(:get => "needs/1/fulfillments").to route_to("fulfillments#index", need_id: '1')
     end
 
     it "routes to #new" do
-      expect(:get => "/fulfillments/new").to route_to("fulfillments#new")
+      expect(:get => "needs/1/fulfillments/new").to route_to("fulfillments#new", need_id: '1')
     end
 
     it "routes to #show" do
-      expect(:get => "/fulfillments/1").to route_to("fulfillments#show", :id => "1")
+      expect(:get => "needs/1/fulfillments/1").to route_to("fulfillments#show", id: '1', need_id: '1')
     end
 
     it "routes to #edit" do
-      expect(:get => "/fulfillments/1/edit").to route_to("fulfillments#edit", :id => "1")
+      expect(:get => "needs/1/fulfillments/1/edit").to route_to("fulfillments#edit", id: '1', need_id: '1')
     end
 
 
     it "routes to #create" do
-      expect(:post => "/fulfillments").to route_to("fulfillments#create")
+      expect(:post => "needs/1/fulfillments").to route_to("fulfillments#create", need_id: '1')
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/fulfillments/1").to route_to("fulfillments#update", :id => "1")
+      expect(:put => "needs/1/fulfillments/1").to route_to("fulfillments#update", id: '1', need_id: '1')
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/fulfillments/1").to route_to("fulfillments#update", :id => "1")
+      expect(:patch => "needs/1/fulfillments/1").to route_to("fulfillments#update", id: '1', need_id: '1')
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/fulfillments/1").to route_to("fulfillments#destroy", :id => "1")
+      expect(:delete => "needs/1/fulfillments/1").to route_to("fulfillments#destroy", id: '1', need_id: '1')
     end
   end
 end
