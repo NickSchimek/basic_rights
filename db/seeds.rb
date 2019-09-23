1f1555fcd2dd7df7a2fb0b8c5bfc10c7a55a57ee
Nick Schimek <cfme00@gmail.com>
2019-09-23 09:47:35 -0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+## Create orgs
 info_211 = Organization.create(name: '211_info', contact: 'Ciara', email: 'ciara@example.org', phone: '(503) 555-5555')
+portland_shelter = Organization.create(name: 'Portland Shelter', contact: 'Bill', email: 'bill@example.org', phone: '(503) 555-0000')
+multnomah_shelter = Organization.create(name: 'Multnomah Shelter', contact: 'Bob', email: 'bob@example.org', phone: '(503) 555-1111')
+washington_shelter = Organization.create(name: 'Washington Shelter', contact: 'Stacey', email: 'stacey@example.org', phone: '(503) 555-2222')
 
-info_211.needs.create(resource: "Children's clothing", description: 'All sizes', quantity: '50')
-info_211.needs.create(resource: "Women's clothing", description: 'All sizes', quantity: '25')
-info_211.needs.create(resource: 'Blankets', description: 'All sizes', quantity: '100')
+orgs = [info_211, portland_shelter, multnomah_shelter, washington_shelter]
 
-info_211.surplus.create(resource: 'Food', description: 'Canned goods', quantity: '500')
-info_211.surplus.create(resource: 'Mens shoes', description: 'All sizes', quantity: '100')
-info_211.surplus.create(resource: "Women's shoes", description: 'Size 10', quantity: '20')
+## Create some needs
+need_childrens_clothing = {resource: "Children's clothing", description: 'All sizes', quantity: '100'}
+need_womans_clothing = {resource: "Women's clothing", description: 'All sizes', quantity: '50'}
+need_blankets = {resource: 'Blankets', description: 'All sizes', quantity: '500'}
+
+needs = [need_childrens_clothing, need_womans_clothing, need_blankets]
+
+
+## Create some surplus
+surplus =
+  [
+    {resource: 'Food', description: 'Canned goods', quantity: '500'},
+    {resource: 'Mens shoes', description: 'All sizes', quantity: '100'},
+    {resource: "Women's shoes", description: 'Size 10', quantity: '200'}
+  ]
+
+orgs.each { |org| needs.each { |need| org.needs.create need } }
+orgs.each { |org| surplus.each { |surplus| org.surplus.create surplus } }
+
+
+## Fulfill some needs
+fulfillments =
+  [
+    { quantity: 20, received: true, active: false, organization_id: portland_shelter.id },
+    { quantity: 20, organization_id: multnomah_shelter.id },
+    { quantity: 10, organization_id: washington_shelter.id }
+  ]
+
+fulfillments.each { |fulfillment| Need.all.each { |need| need.fulfillments.create fulfillment } }
+
+## Claim some surplus
+claims = fulfillments
+claims[0] = { quantity: 20, picked_up: true, active: false, organization_id: portland_shelter }
+claims.each { |claim| Surplu.all.each { |surplus| surplus.claims.create claim } }
