573fe88890e088b42282fd151954d72bb98cd07d
Nick Schimek <cfme00@gmail.com>
2019-10-02 21:35:29 -0700

    Update home page (#53)

--- app/controllers/site_pages_controller.rb
+++ app/controllers/site_pages_controller.rb
@@ -1,5 +1,7 @@
 class SitePagesController < ApplicationController
   def home
+    @needs = Need.all.where(active: true).order(:resource).includes(:organization)
+    @surplus = Surplu.all.where(active: true).order(:resource).includes(:organization)
   end
 
   def about
