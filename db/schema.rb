ab8309d07bc9bec0e701a88b701c0ece8f23f309
Nick Schimek <cfme00@gmail.com>
2019-10-04 19:39:09 -0700

    Update the Role create_table migration to include timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 2019_10_03_200135) do
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_roles_on_name", unique: true
   end
 
