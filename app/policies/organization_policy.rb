25c6048bc71bd13f777f997d8068c2daf64644b4
Nick Schimek <cfme00@gmail.com>
2019-10-04 21:08:39 -0700

    Refactor: Move admin method to user model

--- app/policies/organization_policy.rb
+++ app/policies/organization_policy.rb
@@ -1,19 +1,14 @@
 class OrganizationPolicy < ApplicationPolicy
   def create?
-    site_admin?
+    user.site_admin?
   end
 
   def update?
-    site_admin?
+    user.site_admin?
   end
 
   def destroy?
-    site_admin?
+    user.site_admin?
   end
 
-  private
-    def site_admin?
-      role = Role.find_by(name: 'admin')
-      user.memberships.find_by(role: role)
-    end
 end
