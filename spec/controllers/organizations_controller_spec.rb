969aa52aba60059924b2e5faec975de45dce8996
Nick Schimek <cfme00@gmail.com>
2019-10-04 19:29:19 -0700

    Remove deprecated stub method in controller spec

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -39,10 +39,9 @@ RSpec.describe OrganizationsController, type: :controller do
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # OrganizationsController. Be sure to keep this updated too.
-  # let(:valid_session) { {} }
-  def valid_session
-    controller.stub(:authorize).and_return(true)
-  end
+  let(:valid_session) { {} }
+
+  before(:each) { allow(controller).to receive(:authorize).and_return(true) }
 
   describe "GET #index" do
     it "returns a success response" do
