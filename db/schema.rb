0e39ce0f4521dd469fcb6b40e75063403322cc31
Nick Schimek <cfme00@gmail.com>
2019-10-10 22:50:22 -0700

    Remove phone column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_200135) do
+ActiveRecord::Schema.define(version: 2019_10_11_054824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,7 +90,6 @@ ActiveRecord::Schema.define(version: 2019_10_03_200135) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "encrypted_password", default: "", null: false
