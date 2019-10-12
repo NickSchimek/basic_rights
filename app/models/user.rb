bceb39a114a3fbee387c373ec6b1aafa68d9491e
Nick Schimek <cfme00@gmail.com>
2019-10-11 22:52:10 -0700

    Remove registerable from user model
    This allows us to control who has the ability to add a user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
   validates :name, presence: true
   validates :email, uniqueness: true
   has_many :memberships, dependent: :destroy
