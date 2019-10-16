4d22e92e08623311611b56b05e038ceb176f6bd1
Nick Schimek <cfme00@gmail.com>
2019-10-16 08:22:45 -0700

    Validate organization presence unless superuser

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,4 +2,10 @@ class Membership < ApplicationRecord
   belongs_to :role
   belongs_to :user
   belongs_to :organization, required: false
+
+  validates :organization, presence: true, unless: :superuser?
+
+  def superuser?
+    role_id == Role.find_by(name: 'superuser').id
+  end
 end
--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,9 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Membership, type: :model do
+  let!(:superuser_role) { Role.create(name: 'superuser') }
+  let!(:admin_role) { Role.create(name: 'admin') }
+  let!(:member_role) { Role.create(name: 'member') }
+
   describe 'relationships' do
     it { should belong_to(:user) }
     it { should belong_to(:role) }
     it { should belong_to(:organization).required(false) }
+  end
+
+  describe 'validations' do
+    context 'organization' do
+      let(:superuser) { create(:user, email: 'superuser@example.org') }
+      let(:admin) { create(:user, email: 'admin@example.org') }
+      let(:member) { create(:user, email: 'member@example.org') }
+      it 'allows superuser to persist without belong to an organization' do
+        record = Membership.create(user_id: superuser.id, role_id: superuser_role.id)
+        expect(record).to be_valid
+      end
+
+      it 'does not allow admin to be valid without an organization' do
+        record = Membership.create(user_id: admin.id, role_id: admin_role.id)
+        expect(record).to_not be_valid
+      end
+
+      it 'does not allow member to be valid without an organization' do
+        record = Membership.create(user_id: member.id, role_id: member_role.id)
+        expect(record).to_not be_valid
+      end
+    end
   end
 end
