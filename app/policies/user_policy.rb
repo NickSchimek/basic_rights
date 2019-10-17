6c1ff3e47a4d7e6f714874e4f7cdc4c1ab4a6070
Nick Schimek <cfme00@gmail.com>
2019-10-16 22:58:12 -0700

    Create policy for destroy action
    This allows a superuser or an organizations admin.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -16,4 +16,8 @@ class UserPolicy < ApplicationPolicy
   def create?
     user.superuser? || user.admin?
   end
+
+  def destroy?
+    user.superuser? || user.admin_for?(record)
+  end
 end
--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -78,5 +78,30 @@ RSpec.describe UserPolicy, type: :policy do
       expect(subject).to_not permit(member)
     end
   end
+
+  permissions :destroy? do
+    it 'grants access to the superuser' do
+      Membership.create!(role_id: superuser_role.id, user_id: superuser.id)
+      expect(subject).to permit(superuser)
+    end
+
+    context 'admin' do
+      it 'grants access within their organization' do
+        Membership.create!(role_id: admin_role.id, user_id: admin.id, organization_id: info_211.id)
+        Membership.create!(role_id: member_role.id, user_id: member.id, organization_id: info_211.id)
+        expect(subject).to permit(admin, member)
+      end
+
+      it 'denies access outside of the organization' do
+        Membership.create!(role_id: admin_role.id, user_id: admin.id, organization_id: info_211.id)
+        Membership.create!(role_id: member_role.id, user_id: member.id, organization_id: jones_org.id)
+        expect(subject).to_not permit(admin, member)
+      end
+    end
+
+    it 'denies access to the member' do
+      Membership.create!(role_id: member_role.id, user_id: member.id, organization_id: info_211.id)
+      expect(subject).to_not permit(member, member)
+    end
   end
 end
