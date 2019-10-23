71c4dcbb634537f03f407cba193c9956522744e1
Nick Schimek <cfme00@gmail.com>
2019-10-23 13:23:35 -0700

    Add surplus request specs for 'active' query behavior

--- spec/requests/surplus_spec.rb
+++ spec/requests/surplus_spec.rb
@@ -10,10 +10,37 @@ RSpec.describe "Surplus", type: :request do
   end
 
   describe "GET /surplus" do
-    let!(:surplus) { [create(:surplus, resource: 'surplus_one'), create(:surplus, resource: 'surplus_two')] }
-    it "lists all organizations surplus" do
+    let!(:surplus) { [create(:surplus, resource: 'surplus_one'),
+                      create(:surplus, resource: 'surplus_two'),
+                      create(:surplus, resource: 'surplus_three', active: false)] }
+    it "default view lists active surplus across all organizations" do
       get surplus_path
       expect(response.body).to include('surplus_one', 'surplus_two')
+    end
+
+    it 'default view does not list inactive surplus' do
+      get surplus_path
+      expect(response.body).to_not include('surplus_three')
+    end
+
+    it 'lists active surplus with active query set to true' do
+      get surplus_path, params: { active: true }
+      expect(response.body).to include('surplus_one', 'surplus_two')
+    end
+
+    it 'does not list inactive surplus with active query set to true' do
+      get surplus_path, params: { active: true }
+      expect(response.body).to_not include('surplus_three')
+    end
+
+    it 'lists inactive surplus with active query set to false' do
+      get surplus_path, params: { active: false }
+      expect(response.body).to include('surplus_three')
+    end
+
+    it 'does not list active surplus with active query set to false' do
+      get surplus_path, params: { active: false }
+      expect(response.body).to_not include('surplus_one', 'surplus_two')
     end
   end
 end
