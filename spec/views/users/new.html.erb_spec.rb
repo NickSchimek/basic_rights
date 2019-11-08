f746f7711eb98318ecc4a80622d8d8e80ece11b1
Nick Schimek <cfme00@gmail.com>
2019-11-07 20:46:47 -0800

    Add membership fields to user form
    Added the organization and role fields so that a membership can be
    created during user creation. The view changes whether a superuser or an
    admin is logged in.

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -5,16 +5,55 @@ RSpec.describe "users/new", type: :view do
     assign(:user, build(:user))
   end
 
-  it "renders new user form" do
-    render
+  context "superuser" do
+    it "renders new user form" do
+      super_user = create(:user, email: 'superuser@email.org')
+      super_user.memberships.create role: create(:role, name: 'superuser')
+      allow(view).to receive(:current_user).and_return(super_user)
 
-    assert_select "form[action=?][method=?]", users_path, "post" do
+      render
 
-      assert_select "input[name=?]", "user[name]"
+      assert_select "form[action=?][method=?]", users_path, "post" do
 
-      assert_select "input[name=?]", "user[email]"
+        assert_select "input[name=?]", "user[name]"
 
-      assert_select "input[name=?]", "user[password]"
+        assert_select "input[name=?]", "user[email]"
+
+        assert_select "input[name=?]", "user[password]"
+
+        assert_select "select[name=?]", "organization[id]"
+
+        assert_select "input[type=?]", "radio"
+        assert_select "input[id=?]", "role_name_superuser"
+        assert_select "input[id=?]", "role_name_admin"
+        assert_select "input[id=?]", "role_name_member"
+      end
+    end
+  end
+
+  context "admin" do
+    it "renders new user form" do
+      admin_user = create(:user, email: 'adminuser@email.org')
+      admin_user.memberships.create role: create(:role, name: 'adminuser')
+      allow(view).to receive(:current_user).and_return(admin_user)
+
+      render
+
+      assert_select "form[action=?][method=?]", users_path, "post" do
+
+        assert_select "input[name=?]", "user[name]"
+
+        assert_select "input[name=?]", "user[email]"
+
+        assert_select "input[name=?]", "user[password]"
+
+        assert_select "select[name=?]", "organization[id]", false
+
+        assert_select "input[type=?]", "radio"
+        assert_select "input[id=?]", "role_name_superuser", false
+        assert_select "input[id=?]", "role_name_admin"
+        assert_select "input[id=?]", "role_name_member"
+      end
     end
   end
 end
