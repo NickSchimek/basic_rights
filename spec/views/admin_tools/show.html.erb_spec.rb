1d1c21cfc2cadb704c93f3355cc46de3880a5072
Nick Schimek <cfme00@gmail.com>
2019-11-07 21:56:48 -0800

    Display add user link when admin is logged in
    This update modifies the AdminTool view depending on who is logged in.
    This is done because the admin is not authorized to create new
    organization. Hence, the new organization form is only displayed when a
    superuser is logged in.

--- spec/views/admin_tools/show.html.erb_spec.rb
+++ spec/views/admin_tools/show.html.erb_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe "admin_tools/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "admin_tools/show.html.erb", type: :view do
+  describe 'superuser' do
+    before(:each) do
+      superuser = create(:user)
+      superuser.memberships.create(role: create(:role, name: 'superuser'))
+      assign(:organization, build(:organization))
+      allow(view).to receive(:current_user).and_return(superuser)
+    end
+
+    it 'renders the add user link' do
+      render
+      expect(rendered).to match(new_user_path)
+    end
+
+    it 'renders the organization form' do
+      render
+      expect(rendered).to match('form action="/organizations"')
+    end
+  end
+
+  describe 'admin' do
+    before(:each) do
+      admin = create(:user)
+      admin.memberships.create(role: create(:role, name: 'admin'))
+      allow(view).to receive(:current_user).and_return(admin)
+    end
+
+    it 'renders the add user link' do
+      render
+      expect(rendered).to match(new_user_path)
+    end
+
+    it 'does not render the organization form' do
+      render
+      expect(rendered).to_not match('form action="/organizations"')
+    end
+  end
 end
