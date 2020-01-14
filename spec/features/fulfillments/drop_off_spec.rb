1563b20f6f1e711c20df5f5639a3f216226c8909
Nick Schimek <cfme00@gmail.com>
2020-01-13 21:50:12 -0800

    Use FormBuilder for fulfillment forms

--- spec/features/fulfillments/drop_off_spec.rb
+++ spec/features/fulfillments/drop_off_spec.rb
@@ -4,9 +4,10 @@ include AuthHelper
 describe "new drop off view", type: :feature do
 
   before :each do
-    organization = create(:organization)
+    create(:organization, name: "test_org")
+    @organization = create(:organization)
     @user        = create(:user)
-    @need        = create(:need, organization: organization)
+    @need        = create(:need, organization: @organization)
 
     sign_in(@user)
   end
@@ -21,5 +22,13 @@ describe "new drop off view", type: :feature do
     visit drop_off_new_need_fulfillment_path(@need)
     click_link('Cancel')
     expect(page).to have_current_path(edit_need_path(@need))
+  end
+
+  it "creates a new drop off" do
+    visit drop_off_new_need_fulfillment_path(@need)
+    select('test_org', from: 'Organization')
+    fill_in('Quantity', with: 5)
+    click_button('Add Partial Fulfillment')
+    expect(page).to have_content('successfully created')
   end
 end
